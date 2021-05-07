cd776cdaedf7fefce0f117afc4f52877ae1540df
yishui <727040917@qq.com>
2021-05-07 17:23:47 +0800

    test view questions

---
+++ tests/Feature/ViewQuestionsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewQuestionsTest extends TestCase
+{
+    public function testUserCanViewQuestions()
+    {
+        // 不捕获异常，直接抛出
+        $this->withoutExceptionHandling();
+
+        // 1.假设 /questions 路由存在
+        // 2. 访问链接 questions
+        $test = $this->get('/questions');
+
+        // 3. 正常返回 200
+        $test->assertStatus(200);
+    }
+}
