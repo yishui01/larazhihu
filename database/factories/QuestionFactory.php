de38a96d3a33687fd2ecbd0bbce5e0889e713e54
yishui <727040917@qq.com>
2021-05-07 17:53:44 +0800

    test view single question

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Question::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->text
+        ];
+    }
+}
--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -2,15 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ViewQuestionsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
-    public function UserCanViewQuestions()
+    public function user_can_view_questions()
     {
         // 不捕获异常，直接抛出
         $this->withoutExceptionHandling();
@@ -21,5 +24,20 @@ class ViewQuestionsTest extends TestCase
 
         // 3. 正常返回 200
         $test->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_view_single_question()
+    {
+        // 1. 创建一个问题
+        $question = Question::factory()->create();
+        // 2. 访问链接
+        $test = $this->get('/questions/' . $question->id);
+        // 3. 看到问题内容
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
