e6a32f936aa4334f3695adbc3ea26c68e7541fee
yishui <727040917@qq.com>
2021-12-26 10:11:39 +0800

    extract vote trait

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\VoteTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
     use HasFactory;
+    use VoteTrait;
 
     public $guarded = ["id"];
 
@@ -21,67 +23,9 @@ class Answer extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function getUpVotesCountAttribute()
-    {
-        return $this->votes('vote_up')->count();
-    }
-
-    public function getDownVotesCountAttribute()
-    {
-        return $this->votes('vote_down')->count();
-    }
-
     public function isBest()
     {
         return $this->id == $this->question->best_answer_id;
     }
 
-    public function voteUp($user)
-    {
-        $attributes = ['user_id' => $user->id];
-
-        if (!$this->votes('vote_up')->where($attributes)->exists()) {
-            $this->votes('vote_up')->create(['user_id' => $user->id, 'type' => 'vote_up']);
-        }
-    }
-
-    public function voteDown($user)
-    {
-        $attributes = ['user_id' => $user->id];
-
-        if (!$this->votes('vote_down')->where($attributes)->exists()) {
-            $this->votes('vote_down')->create(['user_id' => $user->id, 'type' => 'vote_down']);
-        }
-    }
-
-    public function cancelVoteUp($user)
-    {
-        $this->votes('vote_up')->where(['user_id' => $user->id, 'type' => 'vote_up'])->delete();
-    }
-
-    public function cancelVoteDown($user)
-    {
-        $this->votes('vote_down')->where(['user_id' => $user->id, 'type' => 'vote_down'])->delete();
-    }
-
-    public function votes($type)
-    {
-        return $this->morphMany(Vote::class, 'voted')->where('type', $type);
-    }
-
-    public function isVotedUp($user)
-    {
-        if (!$user) {
-            return false;
-        }
-        return $this->votes('vote_up')->where('user_id', $user->id)->exists();
-    }
-
-    public function isVotedDown($user)
-    {
-        if (!$user) {
-            return false;
-        }
-        return $this->votes('vote_down')->where('user_id', $user->id)->exists();
-    }
 }
